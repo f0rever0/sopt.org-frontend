631ec31f4139207035f94958b3471a3487e2f7e6
f0ever0 <thddndud0830@gmail.com>
2023-11-24 15:50:19 +0900

    chore : 폴더 구조 변경

--- src/views/MainPage/components/Introduce/index.tsx
+++ src/views/MainPage/components/Introduce/index.tsx
@@ -1,9 +1,11 @@
 import { useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useIsMobile } from '@src/hooks/useDevice';
 import MotionTitle from './MotionTitle';
 import * as S from './style';
 
 export default function Introduce() {
+  const isMobileSize = useIsMobile('768px');
   const contentRef = useRef<HTMLElement>(null);
   const { scrollYProgress } = useScroll({
     target: contentRef,
@@ -13,11 +15,28 @@ export default function Introduce() {
   const scaleValue = useTransform(scrollYProgress, [1, 0.4], [0.9, 1]);
   const content = '전국 최대 규모의 대학생 IT 연합 동아리, SOPT를 소개합니다.';
 
+  const [style, setStyle] = useState<{ opacity?: number; clipPath?: string }>();
+  const scrollValue = useTransform(scrollYProgress, [1, 0.4], ['100%', '0%']);
+
+  useEffect(() => {
+    const unsubscribe = scrollValue.on('change', (value) => {
+      const percentValue = Number(value.split('%')[0]);
+      const newStyle = isMobileSize
+        ? { opacity: (100 - percentValue) / 100 }
+        : { clipPath: `inset(0% ${percentValue}% 0% 0%)`, opacity: 1 };
+      setStyle(newStyle);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollValue, isMobileSize]);
+
   return (
     <S.Background ref={contentRef}>
       <S.Wrapper>
         <S.TextContainer style={{ scale: scaleValue }}>
-          <MotionTitle contentRef={contentRef} content={content} />
+          <MotionTitle style={style} content={content} />
           <S.BackgroundTitle>{content}</S.BackgroundTitle>
         </S.TextContainer>
       </S.Wrapper>
